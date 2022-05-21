883011d08378f2a0e698476438413c4ee0011f5d
Malike01 <81706054+Malike01@users.noreply.github.com>
2022-05-21 17:36:00 +0300

    Update User.java

--- Blood_Donation_Askida_Kan/app/src/main/java/com/example/blood_donation_askida_kan/User.java
+++ Blood_Donation_Askida_Kan/app/src/main/java/com/example/blood_donation_askida_kan/User.java
@@ -1,16 +1,19 @@
 package com.example.blood_donation_askida_kan;
 
 public class User {
-    public String firstName;
-    public String lastName;
-    public String email;
+    public String username;
+    public String blood;
+    public Number phone;
+    public String location;
 
     public User() {
     }
 
-    public User (String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
+    public User (String username, String blood, Number phone, String location) {
+        this.username= username;
+        this.blood = blood;
+        this.phone=phone;
+        this.location =location;
+
     }
 }
