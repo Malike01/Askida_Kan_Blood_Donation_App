a116b060fc9ea313c96599a1b3a82a25effb42e1
Malike01 <81706054+Malike01@users.noreply.github.com>
2022-05-21 15:33:49 +0300

    Update LoginActivity.java

--- Blood_Donation_Askida_Kan/app/src/main/java/com/example/blood_donation_askida_kan/LoginActivity.java
+++ Blood_Donation_Askida_Kan/app/src/main/java/com/example/blood_donation_askida_kan/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.blood_donation_askida_kan;
+/*package com.example.blood_donation_askida_kan;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,4 +18,94 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(LoginActivity.this,DonnorActivity.class);
         startActivity(intent);
     }
+}*/
+package com.example.blood_donation_askida_kan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            finish();
+            return;
+        }
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                authenticateUser();
+            }
+        });
+
+        TextView tvSwitchToRegister = findViewById(R.id.tvSwitchToRegister);
+        tvSwitchToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToRegister();
+            }
+        });
+    }
+
+    private void authenticateUser() {
+        EditText etLoginEmail = findViewById(R.id.email);
+        EditText etLoginPassword = findViewById(R.id.password);
+        EditText etLoginPhone = findViewById(R.id.phone);
+
+        String email = etLoginEmail.getText().toString();
+        String password = etLoginPassword.getText().toString();
+        String phone = etLoginPhone.getText().toString();
+
+        if (email.isEmpty() phone.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password, phone)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            showMainActivity();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Kullanıcı bilgileri hatalı",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void showMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void switchToRegister() {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
+
